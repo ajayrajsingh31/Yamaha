30e1582de6c0ac1063b63acede7fc94f2089802c
ajayrajsingh31 <121920229+ajayrajsingh31@users.noreply.github.com>
2024-09-02 12:12:48 +0530

    Update admin.py

--- yamahadocker/bikes/admin.py
+++ yamahadocker/bikes/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from .models import BikeModel
+
 # Register your models here.
 class BikeAdmin(admin.ModelAdmin):
     list_display = ['bikename']
 
-
-admin.site.register(BikeModel,BikeAdmin)
+# Register the BikeModel with the admin site
+admin.site.register(BikeModel, BikeAdmin)
